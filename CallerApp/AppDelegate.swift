3ded9bbf92687a5c2b8a738d075ed9aa4f479b26
rsudam <raghu.sairam@gmail.com>
2019-04-15 20:51:10 +0530

    created dealer app

--- CallerApp/AppDelegate.swift
+++ CallerApp/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let layout = UICollectionViewFlowLayout()
+        window?.rootViewController =  ViewController(collectionViewLayout: layout)
+        
         return true
     }
 
--- CallerApp/ViewController.swift
+++ CallerApp/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class KeyCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let cellId = "cellId"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView.backgroundColor = .white
+        
+        collectionView.register(KeyCell.self, forCellWithReuseIdentifier: cellId)
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! KeyCell
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let leftRightPadding = view.frame.width * 0.15
+        let innerSpacing = view.frame.width * 0.1
+        let cellWidth = (view.frame.width - 2 * leftRightPadding - 2 * innerSpacing) / 3
+        
+        return .init(width: cellWidth, height: cellWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let leftRightPadding = view.frame.width * 0.15
+        return .init(top: 16, left: leftRightPadding, bottom: 0, right: leftRightPadding)
+    }
 
 }
 
